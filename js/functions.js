58ba10ac11a31a044cb23910f0495e2bbc5cb46d
Camilo <camilogomezr25@gmail.com>
2023-08-17 13:31:32 -0500

    Se configuro el JS para añadir los datos en la tabla

--- js/functions.js
+++ js/functions.js
@@ -4,7 +4,8 @@ $("#nuevoLibro").click(function() {
     var genero = $("#genero").val();
     var codigo = $("#codigo").val();
     var año = $("#año").val();
-    var cantidad = $("#canitidad");
-    $('#libro tr:last').after('<tr><td>' + nombre + '</td><td>' + autor + '</td></tr>'); //agrega un nuevo registro a la tabla, después del último tr
+    var cantidad = $("#cantidad");
+    //agrega un nuevo registro a la tabla, después del último tr:
+    $('#libro tr:last').after('<tr><td>' + nombre + '</td><td>' + autor + '</td><td>' + genero +'</td><td>' + codigo + '</td><td>' +año+'</td><td>'+ cantidad +'</td></tr>'); 
     $('#nuevoLibroModal').modal('toggle'); //cierra el modal
 });
