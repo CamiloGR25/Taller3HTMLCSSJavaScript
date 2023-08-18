04f4f2948c80ce5d79e5297be084ae7a92407c81
xd <1234@hotmail.com>
2023-08-18 15:32:18 -0500

    nueva columna y correccion de eerores

--- js/functions.js
+++ js/functions.js
@@ -6,7 +6,8 @@ $("#nuevoLibro").click(function() {
     var año = $("#año").val();
     var cantidad = $("#cantidad").val();
     var costo= $("#costo").val();
+    var editorial = $("#editorial").val();
     //agrega un nuevo registro a la tabla, después del último tr:
-    $('#libro tr:last').after('<tr><td>' + nombre + '</td><td>' + autor + '</td><td>' + genero +'</td><td>' + codigo + '</td><td>' +año+'</td><td>'+ cantidad +'</td><td>' + costo + '</td></tr>'); 
+    $('#libro tr:last').after('<tr><td>' + nombre + '</td><td>' + autor + '</td><td>' + genero +'</td><td>' + codigo + '</td><td>' +año+'</td><td>'+ cantidad +'</td><td>' + costo + '</td><td>' + editorial + '</td></tr>'); 
     $('#nuevoLibroModal').modal('toggle'); //cierra el modal
 });
