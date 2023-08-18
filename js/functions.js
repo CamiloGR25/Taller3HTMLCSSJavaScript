7bff28fcbf973d55f391dd2a1b4ef669d7f96126
xd <1234@hotmail.com>
2023-08-18 15:13:19 -0500

    Arreglo de fallo de registro de informacion

--- js/functions.js
+++ js/functions.js
@@ -4,7 +4,7 @@ $("#nuevoLibro").click(function() {
     var genero = $("#genero").val();
     var codigo = $("#codigo").val();
     var año = $("#año").val();
-    var cantidad = $("#cantidad");
+    var cantidad = $("#cantidad").val();
     //agrega un nuevo registro a la tabla, después del último tr:
     $('#libro tr:last').after('<tr><td>' + nombre + '</td><td>' + autor + '</td><td>' + genero +'</td><td>' + codigo + '</td><td>' +año+'</td><td>'+ cantidad +'</td></tr>'); 
     $('#nuevoLibroModal').modal('toggle'); //cierra el modal
