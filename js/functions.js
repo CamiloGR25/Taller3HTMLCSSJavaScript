18b4b28daf69066d9c0c51b832b15e1b8cd77042
Gabyto.07 <gabriel7felipebeltran@gmail.com>
2023-08-18 16:03:23 -0500

    Nueva columna descuento corrección

--- js/functions.js
+++ js/functions.js
@@ -7,7 +7,13 @@ $("#nuevoLibro").click(function() {
     var cantidad = $("#cantidad").val();
     var costo= $("#costo").val();
     var editorial = $("#editorial").val();
+    var descuento = $("#descuento").val();
+    
     //agrega un nuevo registro a la tabla, después del último tr:
-    $('#libro tr:last').after('<tr><td>' + nombre + '</td><td>' + autor + '</td><td>' + genero +'</td><td>' + codigo + '</td><td>' +año+'</td><td>'+ cantidad +'</td><td>' + costo + '</td><td>' + editorial + '</td></tr>'); 
+    $('#libro tr:last').after('<tr><td>' + nombre + '</td><td>' + autor + '</td><td>' + genero +'</td><td>' + codigo + '</td><td>' +año+'</td><td>'+ cantidad +'</td><td>' + costo + '</td><td>' + editorial + '</td><td>' + descuento + '</td></tr>'); 
     $('#nuevoLibroModal').modal('toggle'); //cierra el modal
+});
+
+$("#limpiar").click(function() {
+    document.getElementById("#nombre").value= '';
 });
